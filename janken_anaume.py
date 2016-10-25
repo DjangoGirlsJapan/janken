22aa44752ed122159e74094443383aa55b52a518
osoken <osoken.devel@outlook.jp>
2016-10-25 09:05:53 +0900

    shebang は最初の行

--- janken_anaume.py
+++ janken_anaume.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import random
 
@@ -11,18 +11,18 @@ user = input('>>>  ')
 
 try:
     user_choice = """問2：userが入力した数字からdicを使ってグーかチョキかパーの文字を取得してください"""
-    
+
     choice_list = ['1','2','3']
     pc = """問3：import したrandomを使って、PCに数字をチョイスさせて、且つグーかチョキかパーの文字列を取得してください"""
-    
+
     draw = 'DRAW'
     win = 'You Win!!'
     lose = 'You lose!!'
-    
+
     """問4：ユーザの手とPCの手を比較して結果を取得してください
     結果はjudgeという変数を作成して上記のdraw win loseのいずれかを
     変数にいれてください。"""
-    
+
     print(u"あなた選んだのは %s"%user_choice)
     print(u"コンピュータが選んだのは %s"%pc)
     print(u"結果は%s"%judge)
